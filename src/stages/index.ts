587058fad65fb48cb0ecb0fed6ad59c1af96b665
higorseixas <higor.c.seixas@gmail.com>
2023-03-14 13:40:01 -0300

    Add stageThree

--- src/stages/index.ts
+++ src/stages/index.ts
@@ -1,5 +1,7 @@
 import { initialStage } from './0';
 import { stageOne } from './1';
 import { stageTwo } from './2';
+import { stageThree } from './3';
 
-export { initialStage, stageOne, stageTwo };
+
+export { initialStage, stageOne, stageTwo,  stageThree };
