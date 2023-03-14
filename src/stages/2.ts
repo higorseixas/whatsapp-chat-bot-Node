2630423e82d522485f68e48787911b3613cc11b1
Your Name <leonardorocha.limaa@gmail.com>
2023-03-14 19:57:08 -0300

    Trabalhando no stageTwo

--- src/stages/2.ts
+++ src/stages/2.ts
@@ -5,12 +5,15 @@ import { register } from "../responses/register";
 
 export const stageTwo = {
   validateUser(message: string) {
+    const dados = message.split(',')
+    console.log(dados);
     return true;
   },
   exec(exec: stageInterface) {
     if (this.validateUser(exec.message)) {
       storage[exec.from].stage = 6;
       //cadastrar usuário no banco
+      
       return userOptions;
     }else{
       storage[exec.from].stage = 3;
