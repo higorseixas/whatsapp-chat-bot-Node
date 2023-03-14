a05c4ec6ea941aed9d9492dd5f8b2f166d66f03a
Your Name <leonardorocha.limaa@gmail.com>
2023-03-14 14:48:51 -0300

    CRUD do usuário finalizado

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -3,11 +3,77 @@ import { PrismaClient } from "@prisma/client"
 export class UserService {
     constructor(private prisma = new PrismaClient()) { }
 
+    async createUser(
+        cpf: string,
+        nome: string,
+        email: string,
+        telefone: string
+    ) {
+        return await this.getUser(cpf)
+            .then(async (user) => {
+                if (!user) {
+                    return await this.prisma.user.create({
+                        data: {
+                            cpf: cpf,
+                            nome: nome,
+                            email: email,
+                            telefone: telefone
+                        }
+                    })
+                        .then((result) => result)
+                        .catch((error) => console.log(error))
+                } else {
+                    throw new Error('Usuário já existe na base de dados!')
+                }
+            })
+            .catch((error) => console.log(error))
+    }
+
     async getUser(cpf: string) {
-        const user = await this.prisma.user.findUnique({ where: { cpf: '10515154511' } })
+        const user = await this.prisma.user.findUnique({ where: { cpf: cpf } })
             .then((result) => result)
             .catch((error) => console.error(error))
         console.log('userService: ', user);
         return user;
     }
+
+
+    async updateUser(cpf: string,
+        nome: string,
+        email: string,
+        telefone: string) {
+        return await this.getUser(cpf)
+            .then(async (user) => {
+                if (!user) {
+                    throw new Error('Usuário não encontrado!')
+                } else {
+                    return await this.prisma.user.update({
+                        where: { cpf },
+                        data: { nome, email, telefone }
+                    })
+                        .then((result) => result)
+                        .catch((error) => console.log(error))
+                }
+            })
+            .catch((error) => console.log(error))
+    }
+
+    async deleteUser(cpf: string) {
+        return await this.getUser(cpf)
+            .then(async (user) => {
+                if (!user) {
+                    throw new Error('Usuário não encontrado!')
+                } else {
+                    return await this.prisma.user.delete({
+                        where: {
+                            cpf: cpf
+                        }
+                    })
+                        .then((result) => result)
+                        .catch((error) => console.log(error))
+                }
+            })
+            .catch((error) => console.log(error))
+    }
+
 }
