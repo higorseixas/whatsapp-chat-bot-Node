6065dc8c9dc09da02a63f8a6137de97ff8b241c1
Your Name <leonardorocha.limaa@gmail.com>
2023-03-15 16:40:29 -0300

    Trabalhando no stageTwo - Retornando usuário já existente

--- src/stages/2.ts
+++ src/stages/2.ts
@@ -3,28 +3,37 @@ import { storage } from "../storage";
 import { userOptions } from "../responses/userOptions";
 import { register } from "../responses/register";
 import { successRegister } from "../responses/successRegister";
+import { UserService } from '../module/user/user.service';
 
 let returnMessage;
+let userData;
+const userService = new UserService();
 
 export const stageTwo = {
   validateUser(exec: stageInterface) {
-    const data = exec.message.replace(/\s+/g, '').split(',');
-    if (data.length < 4) {//validação dos dados
+    userData = exec.message.replace(/\s+/g, '').split(',');
+    if (userData.length < 4) {//validação dos dados
       returnMessage = '❌ *Dados faltantes.* ❌\n' + register;
       return false
-    } else if (data[0] !== '22222222') {//validação de CPF
+    } else if (userData[0] !== '22222222') {//validação de CPF
       returnMessage = '❌ *CPF inválido.* ❌\n' + register;
       return false
     } else {
       return true;
     }
   },
-  exec(exec: stageInterface) {
+  async exec(exec: stageInterface) {
     const validate = this.validateUser(exec)
     if (validate) {
-      storage[exec.from].stage = 6;
-      //cadastrar usuário no banco
-      return successRegister + userOptions;
+      return await userService.createUser(userData[0], userData[1], userData[2], userData[3])
+        .then(() => {
+          storage[exec.from].stage = 6;
+          return successRegister + userOptions;
+        })
+        .catch((error) => {
+          storage[exec.from].stage = 2;
+          return '❌ Usuário já existe na base de dados! ❌\n' + register;
+        })
     } else {
       storage[exec.from].stage = 2;
       return returnMessage;
