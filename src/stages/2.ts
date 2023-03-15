392a3da8a5886cc458bd242bb8f0aaee28d9cb9d
Your Name <leonardorocha.limaa@gmail.com>
2023-03-15 15:31:38 -0300

    Trabalhando ainda no stageTwo

--- src/stages/2.ts
+++ src/stages/2.ts
@@ -2,22 +2,32 @@ import { stageInterface } from "../interfaces/stageInterface";
 import { storage } from "../storage";
 import { userOptions } from "../responses/userOptions";
 import { register } from "../responses/register";
+import { successRegister } from "../responses/successRegister";
+
+let returnMessage;
 
 export const stageTwo = {
-  validateUser(message: string) {
-    const dados = message.split(',')
-    console.log(dados);
-    return true;
-  },
-  exec(exec: stageInterface) {
-    if (this.validateUser(exec.message)) {
-      storage[exec.from].stage = 6;
-      //cadastrar usuário no banco
-      
-      return userOptions;
-    }else{
-      storage[exec.from].stage = 3;
-      return register
+  validateUser(exec: stageInterface) {
+    const data = exec.message.replace(/\s+/g, '').split(',');
+    if (data.length < 4) {//validação dos dados
+      returnMessage = '❌ *Dados faltantes.* ❌\n' + register;
+      return false
+    } else if (data[0] !== '22222222') {//validação de CPF
+      returnMessage = '❌ *CPF inválido.* ❌\n' + register;
+      return false
+    } else {
+      return true;
     }
   },
+  exec(exec: stageInterface) {
+    const validate = this.validateUser(exec)
+    if (validate) {
+      storage[exec.from].stage = 6;
+      //cadastrar usuário no banco
+      return successRegister + userOptions;
+    } else {
+      storage[exec.from].stage = 2;
+      return returnMessage;
+    }
+  }
 };
