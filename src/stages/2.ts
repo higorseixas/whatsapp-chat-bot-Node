12562508be347d70712ca026f94c4b510fe5ab3f
Your Name <leonardorocha.limaa@gmail.com>
2023-03-15 17:32:22 -0300

    Criando validação - Removendo espaços no inicio e no fim.

--- src/stages/2.ts
+++ src/stages/2.ts
@@ -4,26 +4,36 @@ import { userOptions } from "../responses/userOptions";
 import { register } from "../responses/register";
 import { successRegister } from "../responses/successRegister";
 import { UserService } from '../module/user/user.service';
-import { cpf } from 'cpf-cnpj-validator'; 
+import { cpf } from 'cpf-cnpj-validator';
 
 let returnMessage;
 let userData;
 const userService = new UserService();
 
 export const stageTwo = {
-  validateUser(exec: stageInterface) {
-    userData = exec.message.replace(/\s+/g, '').split(',');
+  removeFirstAndLastSpace(stringArray: string[]): string[] {
+    const result: string[] = [];
+    stringArray.forEach((str: string) => {
+      result.push(str.trim());
+    });
+    return result
+  },
+
+  validateUser(exec: stageInterface): boolean {
+    userData = exec.message.split(',');
+    userData = this.removeFirstAndLastSpace(userData);
     if (userData.length < 4) {//validação dos dados
       returnMessage = '❌ *Dados faltantes.* ❌\n' + register;
       return false
-    } else if (!cpf.isValid(userData[0].replace(/[^a-zA-Z0-9]/g, ''))) {//validação de CPF
+    } else if (!cpf.isValid(userData[0].replace(/[^a-zA-Z0-9]/g, ''))) {//validação de CPF/Remoção de caracteres especiais
       returnMessage = '❌ *CPF inválido.* ❌\n' + register;
       return false
     } else {
       return true;
     }
   },
-  async exec(exec: stageInterface) {
+  
+  async exec(exec: stageInterface): Promise<string> {
     const validate = this.validateUser(exec)
     if (validate) {
       return await userService.createUser(userData[0], userData[1], userData[2], userData[3])
