5c34c49d9353048bcf2251c22b18408947527be9
higorseixas <higor.c.seixas@gmail.com>
2023-03-15 14:51:49 -0300

    stageThree

---
+++ src/responses/userNotFound.ts
@@ -0,0 +1,6 @@
+export const userNotFound =
+    '❌ Usuário não encntrado \n\n' +
+    '1️⃣ - ```Informar CPF novamente ``` \n' +
+    '2️⃣ - ```Cadastrar usuário```\n' +
+    '3️⃣ - ```Encerrar atendimento```\n' +
+    "Para conversar com um de nossos atendentes, *acesse*: https://example.com\n\n"
