1cd7f8bc3f56c63cc38a21d5d22d817cedefd899
Your Name <leonardorocha.limaa@gmail.com>
2023-03-18 00:23:58 -0300

    Testando salvar uma arquivo pdf no banco de dados e enviando via mensagem no whatsapp

--- src/sender.ts
+++ src/sender.ts
@@ -1,6 +1,11 @@
 import { Whatsapp, create, Message, SocketState } from 'venom-bot';
 import { stages, getStage } from './stages';
 
+
+// import { BoletoInterface } from './interfaces/boletoInterface';
+// import { BoletoService } from './module/boleto/boleto.service';
+// import fs from 'fs';
+
 export type QRcode = {
   base64r: string;
   asciiQR: string;
@@ -11,6 +16,7 @@ export class Sender {
   private client: Whatsapp;
   private conected: boolean;
   private qr: QRcode;
+  // private boleto = new BoletoService();
 
   constructor() {
     this.initialize();
@@ -41,6 +47,32 @@ export class Sender {
     };
 
     const start = async (client: Whatsapp) => {
+      //TESTE SALVANDO UM ARQUIVO PDF NO BANCO DE DADOS(BASE64)
+
+      // const pdfBuffer = fs.readFileSync('./dummy.pdf');
+      // const base64String = pdfBuffer.toString('base64');
+      // const boletoConst: BoletoInterface = {
+      //   data: 'data:application/pdf;base64,' + base64String,
+      //   userCpf: '29004950087'
+      // }
+      // this.boleto.createBoleto(boletoConst)
+
+
+      //TESTE - LENDO BASE64 DO BANCO E ENVIANDO VIA MENSAGEM NO WHATSAPP
+      // const pdf = await this.boleto.getBoletoByID(6)
+      // if (pdf?.data) {
+      //   client.sendFileFromBase64(
+      //     '4396753542@c.us',
+      //     pdf?.data,
+      //     'pdfEnviado.pdf',
+      //   )
+      //     .then((result) => {
+      //       console.log('Resultado: ', result); //return object success
+      //     })
+      //     .catch((erro) => {
+      //       console.error('Erro ao enviar: ', erro); //return object error
+      //     });
+      // }
       client.onMessage(async (message) => {
         if (!message.isGroupMsg) {
           const currentStage = getStage({ from: message.from });
